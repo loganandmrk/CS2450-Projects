f15fed406f7fcbe4407ccb996530b844336d69bf
Logan <ljchristiansen2002@gmail.com>
2026-05-14 17:27:21 -0600

    logan

--- main/main.py
+++ main/main.py
@@ -1,2 +1,3 @@
 #please print your name in the format "print('First Last')"
+#and sync the changes to the repository on a new branch "<name>-branch" and create a pull request to merge it into the main branch
 print('Logan Christiansen')
