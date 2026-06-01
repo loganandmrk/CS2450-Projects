50f3882fa22414c2f19a6175a5f201afb5e5224a
loganandmrk <71111892+loganandmrk@users.noreply.github.com>
2026-06-01 16:25:43 -0600

    Revert "hello"

--- Milestone2/memory.py
+++ Milestone2/memory.py
@@ -11,5 +11,3 @@ class Memory:
         self.memory = memory_dict
 
     #define class functions here. we can use the functions.py folder to pull from if needed.
-    def hello():
-        print("hello world")
