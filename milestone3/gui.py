300e9d8c35fc0ca9f39eb0e98573c6751e7e77dc
Logan <ljchristiansen2002@gmail.com>
2026-06-19 16:15:03 -0600

    delted fluff

--- milestone3/gui.py
+++ milestone3/gui.py
@@ -152,18 +152,3 @@ class UVSimGUI:
                 case _:
                     print("Invalid instruction")
             program_counter += 1
-
-
-"""def main():
-    root = tk.Tk()
-    app = UVSimGUI(root)
-    
-    if len(sys.argv) == 2:
-        app.load_file_from_path(sys.argv[1])
-    elif len(sys.argv) > 2:
-        print("Too many arguments presented. Launching GUI empty.")
-        
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()"""
